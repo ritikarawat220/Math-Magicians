938d3bae357b2083a2418b361d2e618949cb95e3
Ritika Rawat <Ritikarawat220@gmail.com>
2023-03-28 19:24:26 +0530

    Add functional component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,12 @@ import './App.css';
 import React from 'react';
 import Calculator from './Components/calculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="container">
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
