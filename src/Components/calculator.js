129e6040a936e5ffe321458c87b4801c7ccec3d0
Ritika Rawat <Ritikarawat220@gmail.com>
2023-03-28 18:27:43 +0530

    Add functional component

--- src/Components/calculator.js
+++ src/Components/calculator.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import Buttons from './Buttons';
 
-class Calculator extends React.PureComponent {
-  render() {
-    return (
-      <div className="cal-operations">
-        <Buttons />
-      </div>
-    );
-  }
+export default function Calculator() {
+  return (
+    <div className="cal-operations">
+      <Buttons />
+    </div>
+  );
 }
-export default Calculator;
