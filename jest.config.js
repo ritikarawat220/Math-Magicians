88b3db62c5b58947882f0c5516ff64450f9094d2
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-05 18:55:00 +0530

    Fix linter errors

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,16 @@
 module.exports = {
-    // ...
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-      '^.+\\.m?js$': 'babel-jest',
-    },
-    transformIgnorePatterns: ['<rootDir>/node_modules/'],
-    testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
-    moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-    globals: {
-      'process.env.NODE_ENV': 'test',
-    },
-    testEnvironment: 'node',
-    // Add the following line to support ECMAScript modules
-    testURL: 'http://localhost',
-  };
-  
+  // ...
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.m?js$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  globals: {
+    'process.env.NODE_ENV': 'test',
+  },
+  testEnvironment: 'node',
+  // Add the following line to support ECMAScript modules
+  testURL: 'http://localhost',
+};
