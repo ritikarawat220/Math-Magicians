4cb47e03cf85373f2b98bf4b9078cf45ed6e84be
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-05 11:40:22 +0530

    Add styling for home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,31 +6,26 @@ const Home = () => (
     <h1>Welcome to our page!</h1>
     <div className={style.paragraphs}>
       <p>
-        A calculator is a useful tool for performing basic
-        mathematical operations quickly and easily.
-        It is a small, handheld device that can be found in
-        nearly every household and office.
-        Calculators can perform basic arithmetic functions like
-        addition, subtraction, multiplication, and division,
-        as well as more advanced functions
-        like percentages and memory storage.
-        In addition to being a tool for performing calculations,
-        a calculator can simplify everyday life in many ways.
-        It can be used for budgeting and managing finances,
-        helping with shopping and comparing prices, and even
-        for cooking and recipe conversions.
+        A calculator is a handy tool for quickly performing basic mathematical
+        calculations. It can save time and effort compared to doing calculations
+        by hand and also reduce the potential for errors. Basic calculators can
+        perform basic mathematical operations such as addition, subtraction,
+        multiplication and division. They are widely used in many fields such as
+        everyday life, business, and education.
+
+        A basic calculator is not just a tool for performing mathematical
+        calculations, it is also a tool for simplifying everyday life.
+        It can help with budgeting and managing finances, assist with
+        shopping and comparing prices, and can even be used for cooking
+        and recipe conversions.
       </p>
       <p>
-        With the advancement of technology, many calculators now have additional
-        features that make them even more versatile and useful.
-        Some calculators have large displays for easy reading,
-        while others are designed to be lightweight and portable.
-        There are also calculators that can perform complex scientific
-        and engineering calculations.
-        In short, a calculator is not just a tool but a companion
-        that can make your day-to-day calculations easier and more accurate.
-        It has become a necessary accessory for many people in fields
-        such as everyday life, business, and education.
+        It is a device that can be found in nearly every household and office,
+        and it has become a necessary accessory for many people. With the advancement
+        of technology, many calculators now have additional features such as memory and
+        percentage functions, making them even more versatile and useful.
+        It&apos;s not just a tool but a companion which can make your day to day
+        calculation easier and accurate.
       </p>
     </div>
   </div>
